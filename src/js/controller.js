1480baa3edacea946be8145fb98f5c186c3d05c2
Ajay Pathak <ap221882@gmail.com>
2022-03-24 17:17:37 +0530

    implementing pagination-2

--- src/js/controller.js
+++ src/js/controller.js
@@ -6,6 +6,7 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // if (module.hot) {
 //   module.hot.accept();
@@ -43,13 +44,22 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query);
     console.log(model.state.search.results);
     resultsView.render(model.getSearchResultsPage());
+    paginationView.render(model.state.search);
   } catch (err) {
     console.log(err);
   }
 };
+
+const controlPagination = function (goToPage) {
+  console.log(goToPage);
+  resultsView.render(model.getSearchResultsPage(goToPage));
+  paginationView.render(model.state.search);
+};
+
 // controlSearchResults();
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
   searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
 };
 init();
